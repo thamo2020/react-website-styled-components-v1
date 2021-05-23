43ff9ccf14369ab428732bceed8c56eb29abd41f
thamo2020 <thamodya01@gmail.com>
2021-05-24 00:01:53 +0530

    Creating Info Sections

---
+++ src/components/InfoSection/InfoSection.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {InfoSec} from './InfoSection.elements';
+
+const InfoSection = ({lightBg}) => {
+    return (
+        <>
+
+        <InfoSec lightBg={lightBg} >
+            h1
+        </InfoSec>
+            
+        </>
+    );
+};
+
+export default InfoSection
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,4 @@
 export {default as Navbar} from './Navbar/Navbar';
+export {default as InfoSection} from './InfoSection/InfoSection';
+
+
